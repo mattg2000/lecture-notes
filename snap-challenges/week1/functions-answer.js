7f4d95e7cf3eb6d4792c6ec41935b41f5a4df7a8
GKephart <gkephart@cnm.edu>
2020-04-21 09:42:03 -0600

    first answer to the snap challenge.

---
+++ snap-challenges/week1/functions-answer.js
@@ -0,0 +1,13 @@
+/**
+ * function that takes two numbers and multiplies them together
+ *
+ * @param number1 number first number to be multiplied
+ * @param number2 number second number to be multiplied
+ * @return number value of number1 times number2
+ **/
+function multiply(number1, number2) {
+	const answer = number1 * number2
+	return answer
+}
+
+console.log(multiply(2,3));
