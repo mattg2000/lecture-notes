13340be4ea9ffe41c5cd33d54aa05de095fece63
GKephart <gkephart@cnm.edu>
2020-04-21 10:05:47 -0600

    added the second answer to the snap challenge

--- snap-challenges/week1/functions-answer.js
+++ snap-challenges/week1/functions-answer.js
@@ -6,8 +6,29 @@
  * @return number value of number1 times number2
  **/
 function multiply(number1, number2) {
+
 	const answer = number1 * number2
+	console.log('answer for multiplication', answer)
 	return answer
 }
+multiply(3,5)
 
-console.log(multiply(2,3));
+/**
+ *  function that takes two strings as arguments and returns a new combined string
+ *
+ * @param string1 first string value to be concatenated
+ * @param string2 second string value to be concatenated
+ * @return string value of the concatenating string1 and string2
+ **/
+
+function concatenatingStrings(string1, string2) {
+	return string1 + ' ' +  string2
+}
+
+let string1 = 'hello'
+let string2 = 'world'
+const combinedString = concatenatingStrings(string1, string2)
+
+console.log('answer for concatenation', combinedString)
+
+
