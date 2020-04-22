b7e7997e1d4a84476ea2853db4fcb0443c62fbb0
GKephart <gkephart@cnm.edu>
2020-04-22 14:56:53 -0600

    added example code for loops.

--- week1/intro-to-algorithims/index.js
+++ week1/intro-to-algorithims/index.js
@@ -1,18 +1,50 @@
-let value = 99
-console.log('left side: ', value < 22)
-console.log('right side: ', value < 100)
-if (value > 22 && value < 100) {
-	console.log('hello there')
-}
+// let value = 99
+// console.log('left side: ', value < 22)
+// console.log('right side: ', value < 100)
+// if (value > 22 && value < 100) {
+// 	console.log('hello there')
+// }
+//
+// if (value === 97) {
+// 	console.log('Value is 97')
+// } else if (value === 98) {
+// 	console.log('Value is 98')
+// } else if (value === 96) {
+// 	console.log('Value is 96')
+// } else {
+// 	console.log(`value is ${value}`)
+// }
 
-if (value === 97) {
-	console.log('Value is 97')
-} else if (value === 98) {
-	console.log('Value is 98')
-} else if (value === 96) {
-	console.log('Value is 96')
-} else {
-	console.log(`value is ${value}`)
-}
+// let number = 0
+//
+// while (number < 9) {
+// 	console.log('Inside of loop', number)
+// 	number = number + 1
+// }
+//
+// console.log('outside of loop', number)
+
+// expected output: 3
 
 
+// let str = "";
+//
+// for (let i = 0; i < 9; i++) {
+// 	console.log('in the for loop',i)
+// 	str = str + i
+// }
+//
+// console.log('final result', str)
+// // expected output: "012345678"
+
+
+
+
+// let array = [1,2,3,4,5,6,7,8,9];
+// let total = 0;
+// array.forEach(function (currentValue) {
+// 	console.log('current value', currentValue)
+// 	total = total + currentValue
+// })
+//
+// console.log(total)
